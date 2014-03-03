ccdef8b88142eabe1179e6fece1600dc9750826c
mtiede <mi.tiede@googlemail.com>
2014-03-03 20:46:14 +0100

    added detail page, changed jQueryMobile to be provided locally in the app

--- www/js/index.js
+++ www/js/index.js
@@ -5,12 +5,51 @@ $('#reposHome').bind('pageinit', function(event) {
 function loadRepos() {
     $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
         var i, repo;
+        $("#allRepos").append("<li data-role=\"list-divider\">JavaScript</li>");
         $.each(data.repositories, function (i, repo) {
             $("#allRepos").append(
-            		"<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+            		"<li><a href='repo-detail.html?owner=" + repo.username + "&name=" + repo.name + "'>"
             		+ "<h4>" + repo.name + "</h4>"
             		+ "<p>" + repo.username + "</p></a></li>");
         });
         $('#allRepos').listview('refresh');
     });
+}
+
+
+$(document).on('pageshow', '#reposDetail', function(event) {
+    var owner = getUrlVars().owner;
+    var name = getUrlVars().name;
+    loadRepoDetail(owner, name);
+});
+
+//$('#reposDetail').live('pageshow', function(event) {
+//    var owner = getUrlVars().owner;
+//    var name = getUrlVars().name;
+//    loadRepoDetail(owner, name);
+//});
+
+function loadRepoDetail(owner,name) {
+    $.ajax("https://api.github.com/repos/" + owner + "/" + name).done(function(data) {
+        var repo = data;
+        console.log(data);
+
+        $('#repoName').html("<a href='" + repo.homepage + "'>" + repo.name + "</a>");
+        $('#description').text(repo.description);
+        $('#forks').html("<strong>Forks:</strong> " + repo.forks + "<br><strong>Watchers:</strong> " + repo.watchers);
+
+        $('#avatar').attr('src', repo.owner.avatar_url);
+        $('#ownerName').html("<strong>Owner:</strong> <a href='" + repo.owner.url + "'>" + repo.owner.login + "</a>");
+    });
+}
+
+function getUrlVars() {
+	var vars = [], hash;
+	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	for (var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	return vars;
 }
